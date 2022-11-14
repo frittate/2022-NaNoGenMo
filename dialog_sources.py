64983b8b72c1334d892fa496d49ba6f94b6f650c
Sebastian Martin <acedia247@gmail.com>
2022-11-14 22:12:38 +0100

    build proper generator under the interior.py file

---
+++ dialog_sources.py
@@ -0,0 +1,57 @@
+DIALOG_SOURCE = [
+    "a radio ",
+    "a TV channel ",
+    "a TV news station ",
+    "a TV program ",
+    "a radio in the next room ",
+    "a news program on a badly received TV channel ",
+    "a voice from a smartphone on a table ",
+    "an automatically playing audio file ",
+    "an undefined sound source in the next room ",
+    "a tiny FM receiver ",
+    "a looping VHS tape ",
+    "a low broadcast from an unidentified source ",
+    "a person on a badly received radio channel ",
+    "a nearby car radio ",
+    "an audio recording  ",
+]
+
+DIALOG_VERB = [
+    "silenty says: ",
+    "whispers: ",
+    "states what could be: ",
+    "mumbles: ",
+    "repeats a line: ",
+    "can be heard saying: ",
+    "mumbles something like: ",
+    "quietly says: ",
+    "whispers something along the lines of: ",
+    "can be heard saying something that might be: ",
+    "indistinctly says something like: ",
+]
+
+DIALOG_SILENCE = [
+    "everything is silent.\n\n\n\n",
+    "nothing makes a sound.\n\n\n\n",
+    "no sound is heard.\n\n\n\n",
+    "there are no sounds.\n\n\n\n",
+    "quiet.\n\n\n\n",
+    "silence.\n\n\n\n",
+    "no noise.\n\n\n\n",
+    "no sound.\n\n\n\n",
+    "nothing else.\n\n\n\n",
+    "almost inaudible insect buzzing.\n\n\n\n",
+    "only a low machine hum.\n\n\n\n",
+]
+
+DIALOG_EMPTY = [
+    "No one is around - ", "It's empty - ", "No one there - ",
+    "No one to be seen - ", "No movement - ", "No one has been here recently - ",
+    "An uncanny emptyness in the space - "
+]
+
+DIALOG_END_VERB = [
+    " can be heard from ", " quietly emits from ", " sounds from ", " comes from ",
+    " is indistinctly said by ", " is mumbled by ", " is whispered by ",
+     " is said in a quiet voice by ", " is said by ", " can be heard from "
+]
--- interior.py
+++ interior.py
@@ -2,86 +2,216 @@ from bs4 import BeautifulSoup
 import requests
 import random
 import re
+import os
 import replicate
+from datetime import datetime
+from dotenv import load_dotenv
 
-STABLE_CLIENT = replicate.Client(api_token="48743e2e94ca24c5016954a617ac5830333afd21")
+import dialog_sources
+
+load_dotenv()
+
+STABLE_CLIENT = replicate.Client(api_token="")
 model = STABLE_CLIENT.models.get("stability-ai/stable-diffusion")
 
 MAIN_LIST = "https://sfy.ru/scripts"
 ALL_HEADERS = []
+ALL_TEXT = []
 REGEX_FOR_NUMBERS = r'[0-9]'
 REGEX_FOR_DOT_WITH_AT_LEAST_TWO_SPACES_AFTER = r'\.\s{2,}'
+REGEX_FOR_UPPERCASE_LETTERS_AND_PUNCTUATION = r'[^A-Z\.\?\!\,\;\-\s]'
+
+SCENE_TAGS = ["INT.", "EXT.", "INTERIOR", "EXTERIOR"]
+LOCATION_TAGS = [" YARD", " BUS", " PORCH"]
+NOW = datetime.now()
 
 i = 0
-MAX_ITEMS = 80
+MAX_ITEMS = 29
+NEWS_PROBABILITY = 18
 
+date_time = NOW.strftime("%Y-%m-%d--%H-%M")
 
-def all_links():
-  content = requests.get(MAIN_LIST).text
-  soup = BeautifulSoup(content, "html.parser")
-  linklist = soup.find("div", { "class": "two-thirds column" })
+FILENAME = "INTERIOR_" + date_time + ".md"
 
-  links = []
-  for link in linklist.findAll("a", href=True):
-    # Format: https://sfy.ru/?script=ace_ventura
-      if "/?script=" in link["href"]:
-          links.append("https://sfy.ru" + link["href"])
+def get_all_links():
+    content = requests.get(MAIN_LIST).text
+    soup = BeautifulSoup(content, "html.parser")
+    linklist = soup.find("div", {"class": "two-thirds column"})
 
-  print("Found number of movies:" + str(len(links)))
-  return links
+    links = []
+    for link in linklist.findAll("a", href=True):
+        # Format: https://sfy.ru/?script=ace_ventura
+        if "/?script=" in link["href"]:
+            links.append("https://sfy.ru" + link["href"])
+
+    with open("links.txt", "w") as f:
+        f.write("\n".join(links))
+
+    print("Found number of movies:" + str(len(links)))
+    return links
+
+def read_links_from_file():
+    with open("links.txt", "r") as f:
+        links = f.read().splitlines()
+    return links
 
 def get_full_text(link):
-  content = requests.get(link).text
-  soup = BeautifulSoup(content, "html.parser")
-  full_text_container = soup.find("pre")
-  full_text = ""
+    content = requests.get(link).text
+    soup = BeautifulSoup(content, "html.parser")
+    full_text_container = soup.find("pre")
+    full_text = ""
 
-  if (full_text_container):
-    full_text = full_text_container.get_text()
+    if (full_text_container):
+        full_text = full_text_container.get_text()
 
-  return full_text
+    return full_text
+
 
 def get_stable_image(prompt_text):
-  return model.predict(prompt=prompt_text, width=256, height=256)
+    print("Prompting AI for: " + prompt_text)
+
+    try:
+        images = model.predict(prompt=prompt_text, width=256, height=256)
+    except replicate.exceptions.ModelError:
+        print("Model error, skipping")
+        images = []
+
+    return images
+
+
+def clean_string(input):
+    input = re.sub(REGEX_FOR_NUMBERS, '', input)
+    input = re.sub(REGEX_FOR_DOT_WITH_AT_LEAST_TWO_SPACES_AFTER, '. ', input)
+    input = re.sub(REGEX_FOR_UPPERCASE_LETTERS_AND_PUNCTUATION, '', input)
+    input = input.replace("INTERIOR", "INT.")
+    input = input.replace("EXTERIOR", "EXT.")
+    input = input.strip()
+    return input
+
 
 def get_scene_headers(link):
-  full_text_for_link = get_full_text(link)
-  full_text_ints = full_text_for_link.split("\n")
+    full_text_for_link = get_full_text(link)
+    full_text_ints = full_text_for_link.split("\n")
 
-  full_text_headers = []
+    full_text_headers = []
 
-  for single_line in full_text_ints:
-    if any(x in single_line for x in ["INT.", "EXT.", "INTERIOR", "EXTERIOR"]):
-      single_line = re.sub(REGEX_FOR_NUMBERS, '', single_line)
-      single_line = re.sub(REGEX_FOR_DOT_WITH_AT_LEAST_TWO_SPACES_AFTER, '. ', single_line)
+    for single_line in full_text_ints:
+        if any(x in single_line for x in SCENE_TAGS):
+            location_line = clean_string(single_line)
+            full_text_headers.append(location_line + "\n\n")
+            """ if any(x in single_line for x in LOCATION_TAGS):
+        for tag in SCENE_TAGS:
+          if tag in single_line:
+            single_line = single_line.replace(tag, "")
 
-      if ("YARD" in single_line):
-        image_urls = get_stable_image(single_line)
-        image_url = image_urls[0]
-        full_text_headers.append("![](" + image_url + ")")
+        image_urls = get_stable_image("HYPERREALISTIC FILM STILL OF " + single_line)
+        if (len(image_urls) > 0):
+          image_url = image_urls[0]
+          full_text_headers.append("![](" + image_url + ")" + "\n\n" + single_line + "\n\n")
 
-      full_text_headers.append(single_line.strip())
+      else: """
 
-  return random.sample(full_text_headers, len(full_text_headers))
 
-links = all_links()
+    return random.sample(full_text_headers, len(full_text_headers))
+
+def get_news():
+    url = "https://api.newscatcherapi.com/v2/search"
+    
+    NEWSCATCHER_TOKEN = os.environ.get("NEWSCATCHER_TOKEN")
+
+    querystring = {
+        "q": "\"climate change\"",
+        "lang": "en",
+        "sort_by": "relevancy",
+        "page": "3",
+        "page_size": 100
+    }
+
+    headers = {"x-api-key": NEWSCATCHER_TOKEN}
+
+    try:
+      print("Getting news...")
+      response = requests.request("GET",
+                                url,
+                                headers=headers,
+                                params=querystring)
+
+    except:
+      print("Error making news request")
+      return []
+
+    json_response = response.json()
+    news_items = json_response["articles"]
+    excerpts = []
+
+    if (json_response["status"] == "ok"):
+      for item in news_items:
+        excerpts.append(item["excerpt"])
+    else:
+      print("Error getting news")
+      
+    return excerpts
+
+def get_and_save_news():
+    news = get_news()
+    if (len(news) > 0):
+      news_string = "\n\n".join(news)
+      with open("news.txt", "a") as f:
+        f.write(news_string)
+
+def generate_lines_from_news():
+    dialog_lines = []
+
+    # news_excerpts = get_news()
+    with open("news.txt", "r") as f:
+      news_excerpts = f.read().split("\n\n")
+
+    for item in news_excerpts:
+      #words_in_item = re.findall(r'\w+', item)
+      words_in_item = item.split(" ")
+      words_in_item = words_in_item[3:-3]
+      
+      dialog_start_1 = random.choice(dialog_sources.DIALOG_SOURCE) + random.choice(dialog_sources.DIALOG_VERB)
+      line_of_dialog_1 = dialog_start_1 + "..." + " ".join(words_in_item) + "..." + "\n\n\n"
+      dialog_lines.append(line_of_dialog_1)
+      
+      dialog_start_2 = random.choice(dialog_sources.DIALOG_END_VERB) + random.choice(dialog_sources.DIALOG_SOURCE)
+      line_of_dialog_2 = "..." + " ".join(words_in_item) + "..." + dialog_start_2 + "\n\n\n"
+      dialog_lines.append(line_of_dialog_2)
+
+    return dialog_lines
+
+# links = get_all_links()
+links = read_links_from_file()
+# get_and_save_news()
+NEWS_DIALOG = generate_lines_from_news()
 shuffled_links = random.sample(links, len(links))
 
 for link in shuffled_links:
-  if (i < MAX_ITEMS):
-    print("Scraping link: " + link)
-    headers_for_link = get_scene_headers(link)
-    ALL_HEADERS.extend(headers_for_link)
-    ALL_HEADERS = random.sample(ALL_HEADERS, len(ALL_HEADERS))
-    i += 1
+    if (i < MAX_ITEMS):
+        # print("Scraping link: " + link)
+        headers_for_link = get_scene_headers(link)
+        ALL_HEADERS.extend(headers_for_link)
+        #ALL_HEADERS = random.sample(ALL_HEADERS, len(ALL_HEADERS))
+        i += 1
 
-with open("INTERIOR.txt", "w", encoding="utf-8") as headersFile:
-    print("Writing to summary file: INTERIOR.md")
-    text_to_write = "\n\n".join(ALL_HEADERS)
-    print(text_to_write)
-    headersFile.write(text_to_write)
+print(len(NEWS_DIALOG), len(ALL_HEADERS))
+
+ALL_TEXT.append("# TELEGRAMS FROM A FUTURE WORLD\n\n")
+
+for i in range(0, len(ALL_HEADERS)):
+  ALL_TEXT.append(ALL_HEADERS.pop(random.randrange(len(ALL_HEADERS))))
+
+  if (random.randint(0, 100) > (100 - NEWS_PROBABILITY) and len(NEWS_DIALOG) > 0):
+    ALL_TEXT.append(NEWS_DIALOG.pop(random.randrange(len(NEWS_DIALOG))))
+  else:
+    ALL_TEXT.append(random.choice(dialog_sources.DIALOG_EMPTY) + random.choice(dialog_sources.DIALOG_SILENCE))
+
+with open(FILENAME, "w", encoding="utf-8") as headersFile:
+    print("Writing to file")
+    text_to_write = ALL_TEXT
+    headersFile.writelines(text_to_write)
     headersFile.close()
-  
 
 
 # extract time designators: DAY, MORNING, EVENING, NIGHT, DAWN, DUSK, SUNSET, SUNDOWN
